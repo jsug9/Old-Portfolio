dff05129892e99a221c10af34aa67e34149e8f39
Augusto <jesus_augga_99@icloud.com>
2022-02-26 12:40:04 -0500

    fix linter errors

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById('myNav').style.width = '100%';
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById('myNav').style.width = '0%';
 }
 
-document.querySelectorAll(".nav-bar-item").forEach(n => n.addEventListener("click", () => {
-  document.getElementById("myNav").style.width = "0%";
-}))
+document.querySelectorAll('.nav-bar-item').forEach(n => n.addEventListener('click', () => {
+  document.getElementById('myNav').style.width = '0%';
+}));
+
+if (window.innerWidth >= 800) {
+  document.getElementById('myNav').style.width = 'content-fit';
+}
