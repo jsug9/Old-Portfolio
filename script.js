4f1ed597559e3d66914c181720c51d3b26d62685
Augusto <jesus_augga_99@icloud.com>
2022-02-28 23:13:28 -0500

    add functionality to see project button so that it shows a dismisable popup window

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ function closeNav() {
   document.getElementById('myNav').style.width = '0%';
 }
 
-function openModal() {
-  document.getElementById('popup-modal').style.width = '100%';
-}
-
-function closeModal() {
-  document.getElementById('popup-modal').style.width = '0%';
-}
-
 // Mobile Menu
 
 document.getElementById('openbtn').addEventListener('click', () => {
@@ -33,10 +25,27 @@ if (window.innerWidth >= 800) {
 }
 
 // Modal Popup
-document.getElementsByClassName('card-button').addEventListener('click', () => {
-  openModal();
-});
+var modal = document.getElementById("popup-modal");
 
-document.getElementById('close-popup').addEventListener('click', () => {
-  closeModal();
-});
+// Get the button that opens the modal
+var btn = document.getElementById("0");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-icon")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
